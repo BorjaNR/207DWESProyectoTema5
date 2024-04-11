8bc3fd18185266666284b88addcf1210b1e41846
BorjaNR <borjanure@gmail.com>
2024-04-11 11:06:18 +0200

    Tema finalizado

--- config/confAPPEE.php
+++ config/confAPPEE.php
@@ -4,4 +4,4 @@
  * @version 2.0
  * @since 09/04/2024
 */
-define('LINK','http://daw207.isauces.local/207DWESProyectoTema5/indexProyectoTema5.html');
+define('LINK','https://daw207.ieslossauces.es/207DWESProyectoTema5/indexProyectoTema5.html');
