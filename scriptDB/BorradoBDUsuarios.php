b2c8b2c0c13e4e0a9bd0918546bbb38867a732b8
BorjaNR <borjanure@gmail.com>
2024-04-11 10:32:08 +0200

    Tema finalizado

---
+++ scriptDB/BorradoBDUsuarios.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Borja Nuñez Refoyos
+ * @version 2.0
+ * @since 11/04/2024
+ * @copyright Todos los derechos reservados Borja Nuñez 
+ * 
+ * @Annotation Scrip de borrado de la base de datos en Explotación
+ * 
+ */
+try {
+    define('DSN', 'mysql:host=db5014806774.hosting-data.io;dbname=dbs12302437'); // Host y nombre de la base de datos
+    define('USERNAME', 'dbu2279609'); // Nombre de usuario de la base de datos
+    define('PASSWORD', 'daw2_Sauces'); // Contraseña de la base de datos
+    // Crear conexión
+    $conn = new PDO(DSN, USERNAME, PASSWORD);
+
+    // Creamos una variable con varias consultas a realizar
+    $consulta = <<<CONSULTA
+            DROP TABLE dbs12302455.T01_Usuario;
+            DROP TABLE dbs12302455.T02_Departamento;
+            DROP TABLE dbs12302455.T06_Animal;
+            CONSULTA;
+    $consultaPreparada = $conn->prepare($consulta);
+    $consultaPreparada->execute();
+
+    echo "<span style='color:green;'>Valores borrados correctamente</span>"; // Mostramos el mensaje si la consulta se a ejecutado correctamente
+} catch (PDOException $miExcepcionPDO) {
+    $errorExcepcion = $miExcepcionPDO->getCode(); // Almacenamos el código del error de la excepción en la variable '$errorExcepcion'
+    $mensajeExcepcion = $miExcepcionPDO->getMessage(); // Almacenamos el mensaje de la excepción en la variable '$mensajeExcepcion'
+
+    echo "<span style='color:red;'>Error: </span>" . $mensajeExcepcion . "<br>"; // Mostramos el mensaje de la excepción
+    echo "<span style='color:red;'>Código del error: </span>" . $errorExcepcion; // Mostramos el código de la excepción
+    die($miExcepcionPDO);
+} finally {
+    // Cerrar la conexión
+    if (isset($conn)) {
+        $conn = null;
+    }
+}
---
+++ scriptDB/CreacionBDUsuarios.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @author Borja Nuñez Refoyos
+ * @version 2.0
+ * @since 11/04/2024
+ * @copyright Todos los derechos reservados Borja Nuñez 
+ * 
+ * @Annotation Script de creación de la base de datos en Explotación
+ * 
+ */
+try {
+    define('DSN', 'mysql:host=db5014806774.hosting-data.io;dbname=dbs12302437'); // Host y nombre de la base de datos
+    define('USERNAME', 'dbu2279609'); // Nombre de usuario de la base de datos
+    define('PASSWORD', 'daw2_Sauces'); // Contraseña de la base de datos
+    // Crear conexión
+    $conn = new PDO(DSN, USERNAME, PASSWORD);
+
+    // Creamos una variable con varias consultas a realizar
+    $consulta = <<<CONSULTA
+            CREATE TABLE IF NOT EXISTS dbs12302455.T01_Usuario (
+                T01_CodUsuario CHAR(8) PRIMARY KEY,
+                T01_Password VARCHAR(64),
+                T01_DescUsuario VARCHAR(255),
+                T01_NumConexiones INT DEFAULT 0,
+                T01_FechaHoraUltimaConexion DATETIME DEFAULT NULL,
+                T01_Perfil ENUM('usuario','administrador') DEFAULT 'usuario',
+                T01_ImagenUsuario BLOB
+            )ENGINE=INNODB;
+            CREATE TABLE IF NOT EXISTS dbs12302455.T02_Departamento (
+                T02_CodDepartamento CHAR(3) PRIMARY KEY,
+                T02_DescDepartamento VARCHAR(255),
+                T02_FechaCreacionDepartamento DATETIME DEFAULT CURRENT_TIMESTAMP,
+                T02_VolumenDeNegocio FLOAT,
+                T02_FechaBajaDepartamento DATETIME
+            )ENGINE=INNODB;
+            CREATE TABLE dbs12302455.T06_Animal (
+                T06_CodAnimal CHAR(3) PRIMARY KEY,
+                T06_DescAnimal VARCHAR(255),
+                T06_FechaNacimiento DATETIME DEFAULT CURRENT_TIMESTAMP,
+                T06_Sexo ENUM('macho','hembra'),
+                T06_Raza VARCHAR(255),
+                T06_Precio DECIMAL(10, 2),
+                T06_FechaBaja DATETIME DEFAULT NULL
+            )ENGINE=INNODB;
+            CONSULTA;
+    $consultaPreparada = $conn->prepare($consulta);
+    $consultaPreparada->execute();
+
+    echo "<span style='color:green;'>Tablas creadas correctamente</span>"; // Mostramos el mensaje si la consulta se a ejecutado correctamente
+} catch (PDOException $miExcepcionPDO) {
+    $errorExcepcion = $miExcepcionPDO->getCode(); // Almacenamos el código del error de la excepción en la variable '$errorExcepcion'
+    $mensajeExcepcion = $miExcepcionPDO->getMessage(); // Almacenamos el mensaje de la excepción en la variable '$mensajeExcepcion'
+
+    echo "<span style='color:red;'>Error: </span>" . $mensajeExcepcion . "<br>"; // Mostramos el mensaje de la excepción
+    echo "<span style='color:red;'>Código del error: </span>" . $errorExcepcion; // Mostramos el código de la excepción
+    die($miExcepcionPDO);
+} finally {
+    // Cerrar la conexión
+    if (isset($conn)) {
+        $conn = null;
+    }
+}
+
+
